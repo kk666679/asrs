30fcca3bc1367b4cb74513c79f0cba08f008bc26
akutuah <akutuahje@gmail.com>
2026-08-08 08:01:04 +0000

    feat: add backend/src/modules/products/products.controller.ts

---
+++ backend/src/modules/products/products.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Put, Delete, Param, Body, Query } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('api/products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll(@Query() query?: any) {
+    return this.productsService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createDto: any) {
+    return this.productsService.create(createDto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateDto: any) {
+    return this.productsService.update(id, updateDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(id);
+  }
+}
