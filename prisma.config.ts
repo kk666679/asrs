d65334a6dc093c6cbc4d10d13d289a65d29ddfc8
akutuah <akutuahje@gmail.com>
2026-08-08 08:03:31 +0000

    feat: add prisma.config.ts

---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+import path from 'node:path';
+import { defineConfig } from 'prisma/config';
+
+const url = process.env.DATABASE_URL ?? 'file:./prisma/dev.db';
+
+export default defineConfig({
+  schema: path.join('prisma', 'schema.prisma'),
+  datasource: {
+    url,
+  },
+});
