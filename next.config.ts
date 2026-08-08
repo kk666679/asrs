3dde60421397f84b015a6d57270ecdd31d75cc24
akutuah <akutuahje@gmail.com>
2026-08-08 08:03:29 +0000

    feat: add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["@tensorflow/tfjs-node"],
+  turbopack: {},
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+    };
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        "@tensorflow/tfjs-node": "commonjs @tensorflow/tfjs-node",
+      });
+    }
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3001/api/:path*",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
