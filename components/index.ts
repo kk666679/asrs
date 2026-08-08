b52285ab6ae5bebfe580b62e61db1a9f61253cab
akutuah <akutuahje@gmail.com>
2026-08-08 08:02:21 +0000

    feat: add components/index.ts

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+// Re-export all UI components
+export * from './ui'
+
+// Re-export specialized components
+export { default as AMRMap } from './AMRMap'
+export { default as AnalyticsDashboard } from './AnalyticsDashboard'
+export { default as BarcodeScanner } from './BarcodeScanner'
+export { default as ItemManager } from './ItemManager'
+export { default as LocationManager } from './LocationManager'
+export { default as RackingMap } from './RackingMap'
+export { default as StorageRetrieval } from './StorageRetrieval'
+export { ThemeProvider } from './theme-provider'
+export { ThemeToggle } from './theme-toggle'
+
+// Re-export shared components
+export * from './shared'
+
